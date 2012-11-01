6b69a7a7ac12c4f18540c963fb520613dc1506e8
hai.wang <380892880@qq.com>
2012-11-01 20:32:24 +0800

    add QLabel on dialog manual

--- t-hellowrld/main.cpp
+++ t-hellowrld/main.cpp
@@ -1,16 +1,25 @@
 
 #include <QtGui>
+//#include <QLabel>
+
 int main(int argc, char* *argv)
 {
     QApplication app(argc, argv);
 
-    QWidget *tt = new QWidget();
+    //QWidget *tt = new QWidget();
     //tt->show();
-    QMainWindow *mm = new QMainWindow();
-    //mm->show();
-    QDialog *dd = new QDialog();
-    dd->show();
 
+    //QMainWindow *mm = new QMainWindow();
+    //mm->show();
+
+    //QDialog *dd = new QDialog();
+    QDialog dd;
+
+    QLabel * label = new QLabel(&dd);
+    label->setText("hello world!");
+
+    //dd->show();
+    dd.show();
 
     return app.exec();
 
